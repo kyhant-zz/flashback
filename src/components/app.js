a9967f2fb95a1508cb10b226657bb85b9d0488ff
Kyhan Turner <kyhanpt@gmail.com>
2016-10-06 18:20:22 -0700

    add styling to splash page

--- src/components/app.js
+++ src/components/app.js
@@ -30,13 +30,24 @@ export default class App extends Component {
     if(this.state.date === null) {
       return (
         <div className='app'>
-      	 <h1>Flashback</h1>
-         <h3>What happened the day you were born?</h3>
-         <div className='date'> 
-          <h6>Select a date to get started</h6>
-          <Date onDateChange={this.articleSearch.bind(this)} />
+      	  <div className="foo">
+            <span className="letter" data-letter="f">f</span>
+            <span className="letter" data-letter="l">l</span>
+            <span className="letter" data-letter="a">a</span>
+            <span className="letter" data-letter="s">s</span>
+            <span className="letter" data-letter="h">h</span>
+            <span className="letter" data-letter="b">b</span>
+            <span className="letter" data-letter="a">a</span>
+            <span className="letter" data-letter="c">c</span>
+            <span className="letter" data-letter="k">k</span>
+            
+            <h3>What happened the day you were born?</h3>
+            <div className='date'> 
+              <h6>Select a date to find out:</h6>
+              <Date onDateChange={this.articleSearch.bind(this)} />
+            </div>
+          </div>
         </div>
-  	   </div>
       )
      } else {
       return (
